adf4a23998df64e73d85367dde04d3ca90a8b92a
bradley2w1dl <bberger@joinhomebase.com>
2025-08-21 22:17:22 -0600

    Update .github/workflows/ci.yml file to run RSpec and Standard for tests and linting respectively

--- app/controllers/oauth_controller.rb
+++ app/controllers/oauth_controller.rb
@@ -3,27 +3,27 @@ class OauthController < ApplicationController
   def authorize
     # This action would handle the OAuth authorization request.
     # It typically involves redirecting the user to a login page or displaying an authorization form.
-    render json: { message: "Authorization endpoint" }
+    render json: {message: "Authorization endpoint"}
   end
 
   # POST /oauth/token
   def token
     # This action would handle the OAuth token request.
     # It typically involves validating the client credentials and issuing an access token.
-    render json: { message: "Token endpoint" }
+    render json: {message: "Token endpoint"}
   end
 
   # POST /introspect
   def introspect
     # This action would handle the introspection of an access token.
     # It typically involves checking the validity of the token and returning its details.
-    render json: { message: "Introspection endpoint" }
+    render json: {message: "Introspection endpoint"}
   end
 
   # POST /oauth/revoke
   def revoke
     # This action would handle the revocation of an access token.
     # It typically involves invalidating the token so it can no longer be used.
-    render json: { message: "Revocation endpoint" }
+    render json: {message: "Revocation endpoint"}
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,16 +3,16 @@ Rails.application.routes.draw do
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
+  get "up" => "rails/health#show", :as => :rails_health_check
 
   # Defines the root path route ("/")
   # root "posts#index"
-  
+
   ## Oauth Routes
   get "authorize" => "oauth#authorize"
-  post "oauth/token" => "oauth#token", as: :oauth_token
+  post "oauth/token" => "oauth#token", :as => :oauth_token
   get "introspect" => "oauth#introspect"
-  post "oauth/revoke" => "oauth#revoke", as: :oauth_revoke
+  post "oauth/revoke" => "oauth#revoke", :as => :oauth_revoke
 
-  get ".well-known/oauth-authorization-server" => "application#well_known_authorization_server", as: :well_known_oauth_authorization_server
+  get ".well-known/oauth-authorization-server" => "application#well_known_authorization_server", :as => :well_known_oauth_authorization_server
 end
