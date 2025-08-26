4ce9ff2eaaa27d800024eeb6fdc81d5424c0b638
bradley2w1dl <bberger@joinhomebase.com>
2025-08-25 21:26:02 -0600

    remove rubocop files -- instead using StandardRb

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,9 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", :as => :rails_health_check
 
   # Defines the root path route ("/")
-  # root "posts#index"
+  # root ""
+
+  resources :users
 
   ## Oauth Routes
   get "authorize" => "oauth#authorize"
