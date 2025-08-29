1eb91e797194cdd8b94e6a9372d3f3e432e9626f
Bradley Berger <bberger@joinhomebase.com>
2025-08-28 22:40:39 -0600

    Bring FE back into the fold (#4)
    * Re-run the rails generator to pull in default Rails FE files
    * Regenerate Gemfile to include FE gems
    
    * remove rubocop files -- instead using StandardRb
    * Add SessionsController and basic Login view
    * Add current_user method to ApplicationController
    * Move :consent endpoint onto the oauth_controller

--- app/controllers/oauth_controller.rb
+++ app/controllers/oauth_controller.rb
@@ -1,10 +1,27 @@
 class OauthController < ApplicationController
   # GET /authorize
+  # @param response_type [String] The type of response expected (e.g., "code" for authorization code flow).
+  # @param client_id [String] The client identifier issued to the client during the registration process.
+  # @param redirect_uri [String] The URI to which the response will be sent after authorization.
+  # @param scope [String] The scope of the access request (e.g., "read", "write").
+  # @param state [String] An opaque value used by the client to maintain state between the request and callback.
+  # @param code_challenge [String] A challenge derived from the code verifier for PKCE (Proof Key for Code Exchange).
+  # @param code_challenge_method [String] The method used to derive the code challenge (e.g., "S256").
+  #
+  # Example response:
+  # If the user authorizes the request, they will be redirected to the redirect_uri with an authorization code:
+  # https://yourapp.com/callback?code=authorization_code&state=random_state_string
+  #
+  # If the user denies the request, they will be redirected to the redirect_uri with an error:
+  # https://yourapp.com/callback?error=access_denied&state=random_state_string
+  #
   def authorize
     # This action would handle the OAuth authorization request.
     # It typically involves redirecting the user to a login page or displaying an authorization form.
     #   - This would need to check for an existing "session token" or JWT in the request.
     #   - if already logged in just redirect back, otherwise show login page first
+    create_auth_code
+
     render json: {message: "Authorization endpoint"}
   end
 
@@ -27,5 +44,23 @@ class OauthController < ApplicationController
     # This action would handle the revocation of an access token.
     # It typically involves invalidating the token so it can no longer be used.
     render json: {message: "Revocation endpoint"}
+  end
+
+  # GET /consent
+  def consent
+    # render the "consent" where user agrees to allow client to acces X scopes
+  end
+
+  private
+
+  def create_auth_code
+    # create an authorization code and store it in the database
+    @auth_code = AuthorizationCode.create(
+      client_id: params[:client_id],
+      redirect_uri: params[:redirect_uri],
+      code_challenge: params[:code_challenge],
+      code_challenge_method: params[:code_challenge_method],
+      scope: params[:scope]&.split(" ")
+    )
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,10 +6,18 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", :as => :rails_health_check
 
   # Defines the root path route ("/")
-  # root "posts#index"
+  # root path...basic "homepage" with ability for an app to register itself...?
+
+  # resources :users
+
+  ## Session Routes
+  get "login" => "sessions#login"
+  post "create_session" => "sessions#create"
+  post "sign_out" => "sessions#destroy"
 
   ## Oauth Routes
   get "authorize" => "oauth#authorize"
+  get "consent" => "oauth#user_consent"
   post "oauth/token" => "oauth#token", :as => :oauth_token
   get "introspect" => "oauth#introspect"
   post "oauth/revoke" => "oauth#revoke", :as => :oauth_revoke
