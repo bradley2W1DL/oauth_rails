bb1b92ca0b04507168993232fb2a30c87ef2c02b
Bradley Berger <bberger@joinhomebase.com>
2025-09-08 21:15:15 -0600

    Fix consent scoping issue with some clever SQL (#10)
    * Add "sample" endpoint to verify more parts of this auth code flow (WIP)

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,14 @@
 class HomeController < ApplicationController
   def index
-    @test_authorize_url = "localhost:3000/authorize?response_type=code&client_id=#{Client.first.client_id}&redirect_uri=http%3A%2F%2Flocalhost%3A3030%2Fclient_app%2Fcallback&scope=read+write&state=xyz"
+    @test_authorize_url = "localhost:3000/authorize?response_type=code&client_id=#{Client.first.client_id}&redirect_uri=http%3A%2F%2Flocalhost%3A3000%2Fsample&scope=read+write&state=xyz"
 
     render :index
+  end
+
+  def sample
+    @state = params[:state]
+    @code = params[:code]
+
+    render :sample
   end
 end
