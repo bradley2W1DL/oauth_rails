d0741343bbe437c0a21b2f28f4edb22722edc0ff
bradley2w1dl <bberger@joinhomebase.com>
2025-09-23 09:01:46 -0600

    Refactor AccessTokenServce and AuthorizationCodeService * the auth code service is just responsible to validating that type of request * access token service is now ready to generate the JWT and return it to controller

--- app/services/oauth/access_token_service.rb
+++ app/services/oauth/access_token_service.rb
@@ -1,21 +1,39 @@
 module Oauth
   class AccessTokenService
-    class << self
-      def generate_token!(grant_type:, client_id:, client_secret: nil, code: nil, code_verifier: nil, redirect_uri:)
-        # really the different grant types are just stating HOW to verify the validity of the request
-        # the actual token creation beyond that is the same, right?
-        # I.e. each service should :validate_request(**params) and then return a boolean or w/e
-        # this service can then continue to actually building the JWT
-        case grant_type
-        when "authorization_code"
-          AuthorizationCodeService.generate_token!(client_id:, client_secret: nil, code:, code_verifier:)
-        when "client_credentials"
-          # TODO implement client credentials flow
-          # ClientCredentialsService.generate_token!
-          raise NotImplementedError, "I haven't gotten around to the client credentials flow yet, sorrrry!"
-        else
-          raise InvalidGrantTypeError, grant_type
-        end
+    attr_reader :grant_type, :client_id, :client_secret, :code, :code_verifier, :redirect_uri
+
+    def initialize(grant_type:, client_id:, client_secret: nil, code: nil, code_verifier: nil, redirect_uri:)
+      @grant_type = grant_type
+      @client_id = client_id
+      @client_secret = client_secret
+      @code = code
+      @code_verifier = code_verifier
+      @redirect_uri = redirect_uri
+    end
+
+    def self.generate_token!(**args)
+      new(**args).generate_token
+    end
+
+    def generate_token!
+      # really the different grant types are just stating HOW to verify the validity of the request
+      # the actual token creation beyond that is the same, right?
+      # I.e. each service should :validate_request(**params) and then return a boolean or w/e
+      # this service can then continue to actually building the JWT
+
+      create_access_token
+    end
+
+    def validate_request!
+      case grant_type
+      when "authorization_code"
+        AuthorizationCodeService.validate!(client_id:, client_secret:, code:, code_verifier:)
+      when "client_credentials"
+        # TODO implement client credentials flow
+        # ClientCredentialsService.validate!(client_id:, client_secret:)
+        raise NotImplementedError, "I haven't gotten around to the client credentials flow yet, sorrrry!"
+      else
+        raise InvalidGrantTypeError, grant_type
       end
     end
   end
